144d1c270c16c8247dee9ada58fda031b2789353
Nathaniel Schlie <schlie.nt@c02w471whtd6.localdomain>
2022-09-21 13:32:14 -0400

    pushing get quote by id

--- main.go
+++ main.go
@@ -32,6 +32,20 @@ var quotesMap = map[string]quote{
 	"2c371688-f482-4c77-943e-89937da93d27": {Id: "2c371688-f482-4c77-943e-89937da93d27", Quote: "Design the architecture, name the components, document the details.", Author: "Tony the Tiger"},
 }
 
+func main() {
+	router := gin.Default()
+	router.GET("/quotes", getRandomQuote)
+	router.GET("/quotes/:id", getQuoteByID)
+	router.Run("0.0.0.0:8080")
+}
+
+// get quote with randomized key and turn into JSON
+func getRandomQuote(c *gin.Context) {
+	quote := quotesMap[getRandomKey()]
+	c.JSON(http.StatusOK, quote)
+}
+
+// make array of keys and pull random key thru random index
 func getRandomKey() string {
 	keyArray := []string{}
 	for k := range quotesMap {
@@ -43,50 +57,13 @@ func getRandomKey() string {
 	return key
 }
 
-func main() {
-	router := gin.Default()
-	router.GET("/quotes", GetQuote)
-	router.Run("0.0.0.0:8080")
+// use id param to search quote map bevcause the keys are ids
+func getQuoteByID(c *gin.Context) {
+	id := c.Param("id")
+	quote, exists := quotesMap[id]
+	if exists {
+		c.JSON(http.StatusOK, quote)
+		return
+	}
+	c.JSON(http.StatusNotFound, gin.H{"message": "matching id not found"})
 }
-
-func GetRandom() quote {
-	quote := quotesMap[getRandomKey()]
-	return quote
-}
-
-func GetQuote(c *gin.Context) {
-	c.JSON(http.StatusOK, GetRandom())
-}
-
-// var quotes = []quote{
-// 	{Quote: "Clear is better than clever.", Author: "Ronald McDonald"},
-// 	{Quote: "Empty string check!", Author: "Squidward Tentacles"},
-// 	{Quote: "Don't panic.", Author: "Oprah Winfrey"},
-// 	{Quote: "A little copying is better than a little dependency.", Author: "Chris Pratt"},
-// 	{Quote: "The bigger the interface, the weaker the abstraction.", Author: "Mary Poppins"},
-// 	{Quote: "With the unsafe package there are no guarantees.", Author: "Rob Dyrdek"},
-// 	{Quote: "Reflection is never clear.", Author: "Bobby Hill"},
-// 	{Quote: "Don't just check errors, handle them gracefully.", Author: "Shrek"},
-// 	{Quote: "Documentation is for users.", Author: "Hermione Granger"},
-// 	{Quote: "Errors are values.", Author: "Clark Kent"},
-// 	{Quote: "Make the zero value useful.", Author: "Drake"},
-// 	{Quote: "Channels orchestrate; mutexes serialize.", Author: "Yo-Yo Ma"},
-// 	{Quote: "Don't communicate by sharing memory, share memory by communicating.", Author: "Prince"},
-// 	{Quote: "Concurrency is not parallelism.", Author: "Lao Tzu"},
-// 	{Quote: "Design the architecture, name the components, document the details.", Author: "Tony the Tiger"},
-// }
-
-// func getQuotes(c *gin.Context) {
-// 	c.IndentedJSON(http.StatusOK, quotes)
-// }
-
-// func main() {
-// 	r := gin.Default()
-// 	r.GET("/quotes", func(c *gin.Context) {
-// 		c.JSON(200, gin.H{
-// 			"quote": getQuote(),
-// 			"author": "Jessie & Nate - Gold Team Rules!",
-// 		})
-// 	})
-// 	r.Run() //default's on 8080
-// }
