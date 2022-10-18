3784c5af4fdd9f65f1c47ea910b53be751eaaf27
Nate <ntschl95@gmail.com>
2022-10-18 11:18:02 -0400

    post functional and delete partially functional

--- gqlgen/graph/model/models_gen.go
+++ gqlgen/graph/model/models_gen.go
@@ -2,6 +2,11 @@
 
 package model
 
+type NewQuote struct {
+	Quote  string `json:"quote"`
+	Author string `json:"author"`
+}
+
 type Quote struct {
 	ID     string `json:"id"`
 	Quote  string `json:"quote"`
--- gqlgen/graph/schema.resolvers.go
+++ gqlgen/graph/schema.resolvers.go
@@ -4,6 +4,7 @@ package graph
 // will be copied through when generating and any unknown code will be moved to the end.
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"io"
@@ -12,6 +13,46 @@ import (
 	"github.com/ntschl/quotes-starter/gqlgen/graph/generated"
 	"github.com/ntschl/quotes-starter/gqlgen/graph/model"
 )
+
+// CreateQuote is the resolver for the createQuote field.
+func (r *mutationResolver) CreateQuote(ctx context.Context, input model.NewQuote) (*model.Quote, error) {
+	quote := &model.Quote{
+		Quote:  input.Quote,
+		Author: input.Author,
+	}
+
+	byteArray, _ := json.Marshal(quote)
+	buffer := bytes.NewBuffer(byteArray)
+
+	request, _ := http.NewRequest("POST", "http://34.160.33.1:80/quotes", buffer)
+	request.Header.Set("x-api-key", "COCKTAILSAUCE")
+
+	client := &http.Client{}
+	response, _ := client.Do(request)
+
+	data, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	json.Unmarshal(data, quote)
+
+	return quote, nil
+}
+
+// DeleteQuote is the resolver for the deleteQuote field.
+func (r *mutationResolver) DeleteQuote(ctx context.Context, id string) (*string, error) {
+	url := "http://34.160.33.1:80/quotes/" + id
+	request, err := http.NewRequest("DELETE", url, nil)
+	request.Header.Set("x-api-key", "COCKTAILSAUCE")
+	if err != nil {
+		return nil, err
+	}
+	client := &http.Client{}
+	client.Do(request)
+	response := "Successful deletion"
+	return &response, nil
+}
 
 // RandomQuote is the resolver for the randomQuote field.
 func (r *queryResolver) RandomQuote(ctx context.Context) (*model.Quote, error) {
@@ -56,7 +97,11 @@ func (r *queryResolver) QuoteByID(ctx context.Context, id string) (*model.Quote,
 	return &quote, nil
 }
 
+// Mutation returns generated.MutationResolver implementation.
+func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
+
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
+type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
