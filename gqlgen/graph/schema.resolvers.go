c86b2e9c32e60a2785ec397521daef7419bd879d
Nate <ntschl95@gmail.com>
2022-10-18 15:40:23 -0400

    added 401 error return to auth

--- gqlgen/graph/schema.resolvers.go
+++ gqlgen/graph/schema.resolvers.go
@@ -7,6 +7,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -30,6 +31,9 @@ func (r *mutationResolver) CreateQuote(ctx context.Context, input model.NewQuote
 
 	client := &http.Client{}
 	response, _ := client.Do(request)
+	if response.Status == "401 Unauthorized" {
+		return nil, errors.New("Error: " + response.Status)
+	}
 
 	data, err := io.ReadAll(response.Body)
 	if err != nil {
@@ -51,6 +55,9 @@ func (r *mutationResolver) DeleteQuote(ctx context.Context, id string) (*string,
 	}
 	client := &http.Client{}
 	response, err := client.Do(request)
+	if response.Status == "401 Unauthorized" {
+		return nil, errors.New("Error: " + response.Status)
+	}
 	if err != nil {
 		return nil, err
 	}
@@ -67,6 +74,9 @@ func (r *queryResolver) RandomQuote(ctx context.Context) (*model.Quote, error) {
 
 	client := &http.Client{}
 	response, _ := client.Do(request)
+	if response.Status == "401 Unauthorized" {
+		return nil, errors.New("Error: " + response.Status)
+	}
 	data, err := io.ReadAll(response.Body)
 	if err != nil {
 		return nil, err
@@ -88,6 +98,9 @@ func (r *queryResolver) QuoteByID(ctx context.Context, id string) (*model.Quote,
 	}
 	client := &http.Client{}
 	response, _ := client.Do(request)
+	if response.Status == "401 Unauthorized" {
+		return nil, errors.New("Error: " + response.Status)
+	}
 	data, err := io.ReadAll(response.Body)
 	if err != nil {
 		return nil, err
